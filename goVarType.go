a91af9f9a9c00b1fd22f502fe89f088cd5878c76
muhammad <muhammad.cse36@gmail.com>
2022-03-06 01:14:21 +0100

    variable global, package level exposed and local

--- goVarType.go
+++ goVarType.go
@@ -2,8 +2,17 @@ package main
 
 import "fmt"
 
+// this upper case I is exposed to other package
+var I int = 100
+
+// this j is global but within this package
+var j int = 20
+
 func main() {
+	// this i is local variable and not exposed
 	var i float64
 	i = 40.0
+	j = I + j
+
 	fmt.Printf("%v %T", i, i)
 }
